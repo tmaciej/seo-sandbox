47af9d79836ff83e0fea52a57c6578e56cc6fd30
front4 <front4@a.dev.int>
2016-11-08 14:56:09 +0100

    Async testing with timeouts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      after100: <p className="red">Loading content after 100ms...</p>,
+      after1000: <p className="red">Loading content after 1000ms...</p>,
+      after10000: <p className="red">Loading content after 10000ms...</p>
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        after100: <p className="green">This was loaded after 100ms.</p>
+      });
+    }, 100);
+
+    setTimeout(() => {
+      this.setState({
+        after1000: <p className="green">This was loaded after 1000ms.</p>
+      });
+    }, 1000);
+
+    setTimeout(() => {
+      this.setState({
+        after10000: <p className="green">This was loaded after 10000ms.</p>
+      });
+    }, 10000);
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <p>Static content.</p>
+        {this.state.after100}
+        {this.state.after1000}
+        {this.state.after10000}
       </div>
     );
   }
